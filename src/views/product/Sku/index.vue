bc6756e68eff74a7110cb1bf1f3b0a9ce00bd459
Kyle845 <738779095@qq.com>
2023-03-09 18:23:15 +0800

    逻辑补充

--- src/views/product/Sku/index.vue
+++ src/views/product/Sku/index.vue
@@ -53,12 +53,38 @@
               layout="prev, pager, next, jumper,->, sizes,total"
               :total="total">
         </el-pagination>
-        <el-drawer :visible.sync="show" :show-close="true" size="50%">
-              <el-row>
-                    <el-col :span="5">名称</el-col>
-                    <el-col :span="10">sku</el-col>
-              </el-row>
-        </el-drawer>
+        <el-drawer  :visible.sync="show" :show-close="false" size="50%">
+      <el-row>
+      <el-col :span="5">名称</el-col>
+      <el-col :span="16">{{skuInfo.skuName}}</el-col>
+    </el-row>
+       <el-row>
+      <el-col :span="5">描述</el-col>
+      <el-col :span="16">{{skuInfo.skuDesc}}</el-col>
+    </el-row>
+       <el-row>
+      <el-col :span="5">价格</el-col>
+      <el-col :span="16">{{skuInfo.price}}元</el-col>
+    </el-row>
+       <el-row>
+      <el-col :span="5">平台属性</el-col>
+      <el-col :span="16">
+        <template>
+            <el-tag type="success" v-for="(attr,index) in skuInfo.skuAttrValueList" :key="attr.id" style="margin-right:10px">{{attr.attrId}}-{{attr.valueId}}</el-tag>
+        </template>
+      </el-col>
+    </el-row>
+       <el-row>
+      <el-col :span="5">商品图片</el-col>
+      <el-col :span="16">
+        <el-carousel height="150px">
+         <el-carousel-item v-for="item in skuInfo.skuImageList" :key="item.id">  
+               <img :src="item.imgUrl">
+        </el-carousel-item>
+    </el-carousel>
+      </el-col>
+    </el-row>
+    </el-drawer>
     </div>
 </template>
 <script>
@@ -109,11 +135,47 @@ export default {
     edit(){
 
     },
-    getSkuInfo(){
-
+    async getSkuInfo(sku){
+        this.show = true;
+        let result = await this.$API.sku.reqSkuById(sku.id);
+        if (result.code == 200) {
+          this.skuInfo = result.data;
+        }
     }
   },
 }
 </script>
-<style lang="">
+<style>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
+  }
+</style>
+
+<style scoped>
+   .el-row .el-col-5{
+      font-size:18px;
+      text-align:right;
+   }
+   .el-col{
+     margin:10px 10px;
+   }
+
+   >>>.el-carousel__button{
+    width:10px;
+    height:10px;
+    background:red;
+    border-radius:50%;
+  }
 </style>
